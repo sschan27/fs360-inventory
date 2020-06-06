1aed7009b6177adb5fcd99a449b6134c924c9f21
sschan27 <sschan27@gmail.com>
2020-06-06 18:27:34 +0800

    change color

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -20,7 +20,7 @@
         const chartColors = {
             red: 'rgb(255, 100, 132)',
             orange: 'rgb(255, 159, 64)',
-            yellow: 'rgb(255, 205, 86)',
+            yellow: 'rgb(255, 220, 86)',
             green: 'rgb(75, 192, 192)',
             blue: 'rgb(54, 162, 235)',
             purple: 'rgb(153, 102, 255)',
